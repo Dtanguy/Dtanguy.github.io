6a2a588f423863b565fc56e7d430e1d9debbd8b7
Tanguy <pogonas91@hotmail.fr>
2016-10-26 17:31:38 +0200

    Clone
    Clone my portfolio from old OVH vps

---
+++ my_js.js
@@ -0,0 +1,20 @@
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85971918-1', 'auto');
+  ga('send', 'pageview');
+
+
+
+
+function show_hide_div(div_id){
+    if( $(div_id).css("display")=="none"){
+        $(div_id).css("display", "block");
+    }else {
+        $(div_id).css("display", "none");
+    }
+}
